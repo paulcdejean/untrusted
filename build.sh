b11997e59f2e3f71f9d1ee0919255104af5817e2
unknown <mdejean@actul307-dd50.CLEVELANDACTU.local>
2014-04-16 15:59:08 -0400

    install script and local jquery (for file:///)

---
+++ build.sh
@@ -0,0 +1,26 @@
+export target="scripts/build/untrusted.js"
+
+export modules="scripts/util.js 
+			 scripts/_head.js 
+             scripts/game.js 
+             scripts/codeEditor.js 
+             scripts/display.js 
+             scripts/dynamicObject.js 
+             scripts/inventory.js 
+             scripts/map.js 
+             scripts/objects.js 
+             scripts/player.js 
+             scripts/reference.js 
+             scripts/sound.js 
+             scripts/validate.js 
+             scripts/ui.js 
+	         levels/levels.js 
+             scripts/_launcher_release.js 
+	         scripts/_tail.js"
+
+echo "Building level file...\t\t\t\c"
+./compile_levels.sh
+echo "[ Done ]"
+echo "Merging JS files...\t\t\t\c"
+cat $modules > $target
+echo "[ Done ]"
