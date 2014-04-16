e718709baabeed2a1c7663ca7adc8915805ef1a5
unknown <mdejean@actul307-dd50.CLEVELANDACTU.local>
2014-04-16 17:22:14 -0400

    prevent errors in commit by rerunning in temp

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
-./build.sh
-git checkout gh-pages
-git merge master --no-commit 
-git push origin gh-pages
-git checkout master
+#!/bin/sh 
+if [ $0 != "/tmp/deploy.sh" ]; then
+    cp $0 /tmp/deploy.sh;
+    /tmp/deploy.sh &
+    exit;
+fi
+set -e
+git checkout gh-pages;
+git merge master --no-commit;
+git push origin gh-pages;
+git checkout master;
