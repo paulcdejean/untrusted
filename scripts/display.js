02f9e7cb27b42f346ee2db651d018a2ede770054
unknown <mdejean@actul307-dd50.CLEVELANDACTU.local>
2014-04-17 12:22:24 -0400

    let us begin

--- scripts/display.js
+++ scripts/display.js
@@ -163,9 +163,7 @@ ROT.Display.prototype.playIntro = function (map, i) {
         this.clear();
         this.drawText(0, i - 2, "%c{#0f0}> initialize")
         this.drawText(15, i + 3, "U N T R U S T E D");
-        this.drawText(20, i + 5, "- or - ");
-        this.drawText(5, i + 7, "THE CONTINUING ADVENTURES OF DR. EVAL");
-        this.drawText(3, i + 12, "a game by Alex Nisnevich and Greg Shuflin");
+        this.drawText(17, i + 5, "redefinition");
         this.drawText(10, i + 22, "Press any key to begin ...")
         setTimeout(function () {
             display.playIntro(map, i - 1);
