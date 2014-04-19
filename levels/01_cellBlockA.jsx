2c1d680097f20288d4981e41f37c93a57aa20253
root <root@ip-10-185-227-233.ec2.internal>
2014-04-19 06:41:20 +0000

    Modded level 1.

--- levels/01_cellBlockA.jsx
+++ levels/01_cellBlockA.jsx
@@ -1,6 +1,6 @@
 #BEGIN_PROPERTIES#
 {
-    "version": "1.2",
+    "version": "1.3.1",
     "commandsIntroduced":
         ["global.startLevel", "global.onExit", "map.placePlayer",
          "map.placeObject", "map.getHeight", "map.getWidth",
@@ -12,30 +12,22 @@
  * cellBlockA.js *
  *****************
  *
- * Good morning, Dr. Eval.
- *
- * It wasn't easy, but I've managed to get your computer down
- * to you. This system might be unfamiliar, but the underlying
- * code is still JavaScript. Just like we predicted.
- *
- * Now, let's get what we came here for and then get you out of
- * here. Easy peasy.
- *
- * I've given you as much access to their code as I could, but
- * it's not perfect. The red background indicates lines that
- * are off-limits from editing.
- *
- * The code currently places blocks in a rectangle surrounding
- * you. All you need to do is make a gap. You don't even need
- * to do anything extra. In fact, you should be doing less.
+ * Hey Marcel. I've eliminated some of the more obvious ways
+ * you can cheat yourself out of this box.
+ * This should still be a simple puzzle though, even with the
+ * extra restrictions you'll have to deal with.
  */
 
 function startLevel(map) {
 #START_OF_START_LEVEL#
-    map.displayChapter('Chapter 1\nBreakout');
+    map.displayChapter('Chapter 1\nSimple box');
 
     map.placePlayer(7, 5);
+
 #BEGIN_EDITABLE#
+    // You get your computer in this level so it should be easy :D
+    map.placeObject(15, 12, 'computer');
+#END_EDITABLE#
 
     for (y = 3; y <= map.getHeight() - 10; y++) {
         map.placeObject(5, y, 'block');
@@ -46,13 +38,20 @@ function startLevel(map) {
         map.placeObject(x, 3, 'block');
         map.placeObject(x, map.getHeight() - 10, 'block');
     }
-#END_EDITABLE#
-
-    map.placeObject(15, 12, 'computer');
 
     map.placeObject(map.getWidth()-7, map.getHeight()-5, 'exit');
 #END_OF_START_LEVEL#
 }
+
+function validateLevel(map) {
+    // No creating exits!
+    map.validateExactlyXManyObjects(1, 'exit');
+
+    // No fancy items!
+    map.validateExactlyXManyObjects(0, 'phone');
+    map.validateExactlyXManyObjects(0, 'theAlgorithm');
+}
+
 
 function onExit(map) {
     if (!map.getPlayer().hasItem('computer')) {
