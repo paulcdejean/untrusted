6c8abfa3de7c7fa0dec5c32934ec7c011801f5eb
mdejean <mrd79@case.edu>
2014-04-20 13:42:22 -0400

    well, it's a level.

--- levels/01_redefinition.jsx
+++ levels/01_redefinition.jsx
@@ -1,18 +1,19 @@
 #BEGIN_PROPERTIES#
 {
-    "version": "1.2",
+    "version": "1.0001",
     "commandsIntroduced":
         ["global.startLevel", "global.onExit", "map.placePlayer",
          "map.placeObject", "map.getHeight", "map.getWidth",
+         "map.defineObject", "player.getColor", "player.setColor",
          "map.displayChapter", "map.getPlayer", "player.hasItem"],
     "music": "The Green"
 }
 #END_PROPERTIES#
 /*****************
- * cellBlockB.js *
+ * redefinition.js *
  *****************
  *
- * Lets try something a little less... trivial
+ * <insert flavor text here>
  *
  */
 
@@ -21,25 +22,60 @@ function startLevel(map) {
     map.displayChapter('Chapter 1\nBreakout');
 
     map.placePlayer(7, 5);
+    
+    map.placeObject(15, 12, 'computer');
+    
+    for (y = 10; y <= 14; y++) {
+        map.placeObject(12, y, 'block');
+        map.placeObject(18, y, 'block');
+        map.placeObject(22, y, 'block');
+        map.placeObject(28, y, 'block');
+    }
+
+    for (x = 12; x <= 16; x++) {
+        map.placeObject(x, 10, 'block');
+        map.placeObject(x, 14, 'block');
+    }
+    
+    for (x = 22; x <= 26; x++) {
+        map.placeObject(x, 10, 'block');
+        map.placeObject(x, 14, 'block');
+    }
+    
+    map.defineObject('door', {
+      'symbol': '-',
+      'color': '#0f0',
+      'impassable': function (player) {
+        if (player.getColor() == this.color) {
+          player.setColor('red');
+          return false;
+        }
+        return true;
+      }
+    });
+    
+    map.placeObject(17, 10, 'door');
+    map.placeObject(17, 14, 'block');
+    
+    map.placeObject(27, 10, 'door');
+    map.placeObject(27, 14, 'block');
 #BEGIN_EDITABLE#
 
-    for (y = 3; y <= map.getHeight() - 10; y++) {
-        map.placeObject(5, y, 'block');
-        map.placeObject(map.getWidth() - 5, y, 'block');
-    }
-
-    for (x = 5; x <= map.getWidth() - 5; x++) {
-        map.placeObject(x, 3, 'block');
-        map.placeObject(x, map.getHeight() - 10, 'block');
-    }
 #END_EDITABLE#
 
-    map.placeObject(15, 12, 'computer');
-
-    map.placeObject(map.getWidth()-7, map.getHeight()-5, 'exit');
+    map.placeObject(25, 12, 'exit');
 #END_OF_START_LEVEL#
 }
 
 function validateLevel(map) {
     map.validateExactlyXManyObjects(1, 'exit');
+}
+
+function onExit(map) {
+    if (!map.getPlayer().hasItem('computer')) {
+        map.writeStatus("Don't forget to pick up the computer!");
+        return false;
+    } else {
+        return true;
+    }
 }
